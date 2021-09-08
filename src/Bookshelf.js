aeada034f4cae7ed599c3e75ad803624935d8a83
tjbernard <tjbenad@gmail.com>
2021-09-08 21:52:06 +0300

    Update Bookshelf.js

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,83 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import BookCover from './BookCover';
 
-class Bookshelf extends Component {
-    static propTypes = {
-        onMoveBook: PropTypes.func.isRequired,
-    }
-
-    moveBook = (book, shelf) => {
-        const { onMoveBook } = this.props;
+const Bookshelf = (props) =>{
+    
+    const moveBook = (book, shelf) => {
+        const { onMoveBook } = props;
         onMoveBook(book, shelf);
     }
 
-    render() {
-        const { books, isLoading } = this.props;
-        
-        return(
-            <div>
-                {isLoading && (
-                    <div className="page-loading">
-                        <span>Loading...</span>
-                    </div>
-                )}
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter((b) => b.shelf === 'currentlyReading').length > 0
-                                ? books.filter((b) => b.shelf === 'currentlyReading').map((book, i) => (
-                                    <BookCover
-                                        bookInfo={book}
-                                        key={i}
-                                        onMoveBook={this.moveBook}
-                                    />
-                                ))
-                                : <span>No books</span>
-                            }
-                        </ol>
-                    </div>
-                </div>
+    const { books, isLoading } = props;
+    const currentReading = books.filter((b) => b.shelf === 'currentlyReading');
+    const wantToRead = books.filter((b) => b.shelf === 'wantToRead');
+    const read = books.filter((b) => b.shelf === 'read');
 
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter((b) => b.shelf === 'wantToRead').length > 0
-                                ? books.filter((b) => b.shelf === 'wantToRead').map((book, i) => (
-                                    <BookCover
-                                        bookInfo={book}
-                                        key={i}
-                                        onMoveBook={this.moveBook}
-                                    />
-                                ))
-                                : <span>No books</span>
-                            }
-                        </ol>
-                    </div>
-                </div>
-
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter((b) => b.shelf === 'read').length > 0
-                                ? books.filter((b) => b.shelf === 'read').map((book, i) => (
-                                    <BookCover
-                                        bookInfo={book}
-                                        key={i}
-                                        onMoveBook={this.moveBook}
-                                    />
-                                ))
-                                : <span>No books</span>
-                            }
-                        </ol>
-                    </div>
+    const shelf = (books, heading) => {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{heading}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.length > 0
+                            ? books.map((book, i) => (
+                                <BookCover
+                                    bookInfo={book}
+                                    key={i}
+                                    onMoveBook={moveBook}
+                                />
+                            ))
+                            : <span>No books</span>
+                        }
+                    </ol>
                 </div>
             </div>
-        );
+        )
     }
+    
+    return(
+        <div>
+            {isLoading && (
+                <div className="page-loading">
+                    <span>Loading...</span>
+                </div>
+            )}
+            {shelf(currentReading, 'Current Reading')}
+            {shelf(wantToRead, 'Want To Read')}
+            {shelf(read, 'Read')}
+        </div>
+    );
 }
 
 export default Bookshelf;
+
+Bookshelf.propTypes = {
+    onMoveBook: PropTypes.func.isRequired,
+}
